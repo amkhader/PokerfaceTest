a33291f7d4a10693b98465551b93a4a50b32edcc
amkhader <alaa.m.khader@gmail.com>
2016-01-10 03:59:35 +0300

    update

--- Media/parseArabicSrt.py
+++ Media/parseArabicSrt.py
@@ -5,8 +5,14 @@ file = codecs.open('PhasesofMeiosisI.ar.srt','r', encoding = 'utf-8')
 
 fileOut = codecs.open('PhasesofMeiosisI.txt','w', encoding = 'utf-8')
 
+count = 0
+
 for line in file:
     if (line[0] not in ['1','2','3','4','5','6','7','8','9','0']):
-        fileOut.write(line)
+        if not line.strip() :
+            count +=1
+        else:
+            fileOut.write(str(count) + " " +line)
 
+print count
 file.close
