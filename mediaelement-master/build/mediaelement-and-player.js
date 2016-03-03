b5c37f02a613518997cd1dd6363c82bd11fa64e6
amkhader <alaa.m.khader@gmail.com>
2016-03-03 13:24:52 +0300

    Update mediaelement-and-player.js

--- mediaelement-master/build/mediaelement-and-player.js
+++ mediaelement-master/build/mediaelement-and-player.js
@@ -3047,7 +3047,7 @@ if (typeof jQuery != 'undefined') {
 						var htmlQ1 = '<h1 id="question">Are you confused by something in this frame?</h1>';
 						var htmlQ2 = '<h1>Are you confused by jargon?</h1>';
 						var htmlButtons = '<style>#button1{ width: 300px; height: 40px;} #button2{ width: 300px; height: 40px;} #container{ text-align: center;}</style>';
-						var htmlButtonsRewind = '<div id="container"><button onclick="this.close()" id="button1">YES</button> <button onclick="rewindFunction()" id = "button2">NO, REWIND</button></div>';
+						var htmlButtonsRewind = '<div id="container"><button onclick=".close()" id="button1">YES</button> <button onclick="rewindFunction()" id = "button2">NO, REWIND</button></div>';
 						var RewindFun = '<script>function rewindFunction() {document.getElementById("player1").currentTime = track.entries.times[currentSubNum-1]["start"]; }</script>';
 						var htmlButtonsJargon = '<div id="container"><button onclick="jargonFunction()" id="button1">YES</button> <button id = "button2">NO</button></div><script>function jargonFunction() {document.getElementById("container").innerHTML ="This word means..."; }</script>';
 						//var JargonFun = '<script>function jargonpopup(){var j = $.inArray(currentSubNum, JargonSubNum);if (j > -1){htmlQ2 = "<h1>Are you confused by ".concat(JargonWords[j]).concat("?</h1>");document.getElementById("question").innerHTML =htmlQ2;document.getElementById("container").innerHTML = "<div id="container1"><button onclick="jargonFunction()" id="button1">YES</button> <button id = "button2">NO</button></div><script>function jargonFunction() {document.getElementById("container1").innerHTML ="This word means..."; }</script>";}}'
