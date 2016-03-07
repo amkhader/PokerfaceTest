8d9fe62af5c3a0007b481031cc2f843cdad6dd37
amkhader <alaa.m.khader@gmail.com>
2016-03-08 00:45:22 +0300

    Update mediaelement-and-player.js

--- mediaelement-master/build/mediaelement-and-player.js
+++ mediaelement-master/build/mediaelement-and-player.js
@@ -3063,7 +3063,7 @@ if (typeof jQuery != 'undefined') {
 						var MTEpopup = '<script>function MTEpopup() {$("h1").html("هل تشعر الخلط من جانب الترجمة؟")  ;$("#container").html("<button onclick = \'MTEFunction()\' id= \'button1\'>نعم</button> <button onclick = \'Textpopup()\' id = \'button2\'>لا</button>");} </script>';
 						var MTEFunction = '<script>function MTEFunction() {document.getElementById("container").innerHTML =\'This could also be translated:...\';}</script>';
 						var Textpopup = '<script>function Textpopup() {$("h1").html(\'What are you confused by?\');$("#container").html(\'<textarea id = \"textArea\">I am confused by </textarea><button onclick="savetextarea()" type="button">Submit</button>\');}</script>';
-						var SaveInput = '<script>function savetextarea(){var txt = document.getElementById("textArea").value;csvResult.push("Other: " + txt)</script>';
+						var SaveInput = '<script>function savetextarea(){var txt = document.getElementById("textArea").value;csvResult.push("Other: " + txt)}</script>';
 						var ResultsLink = '<script>var csvContent = "data:text/csv;charset=utf-8,"; csvContent += csvResults.join("\n");var encodedUri = encodeURI(csvContent);console.log("download this results link: " + encodedUri);</script>';
 						//<button onclick = \'jargonFunction()\' id= \'button11\'>YES</button> <button id = \'button22\'>NO</button>
 						//
