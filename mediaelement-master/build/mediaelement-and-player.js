7ffd5be45ef44c98b7595af43e4b7ec35b68b582
amkhader <alaa.m.khader@gmail.com>
2016-04-13 14:29:35 +0300

    Update mediaelement-and-player.js

--- mediaelement-master/build/mediaelement-and-player.js
+++ mediaelement-master/build/mediaelement-and-player.js
@@ -46,7 +46,7 @@ var jargon = "";
 //$.get("mediaelement-master/build/jargonList.txt", function(data){items = data.split('\n'); });
 
 $.ajax({
-      url: "Subtitles/Astronomy/Interferometry_Sizing_Up_the_Stars_1_jargon.txt",
+      url: "Subtitles/Math/Discrete_Logarithm_Problem_jargon.txt",
       success: function (data){JargonData = data.split('\n'); 
       	for (i = 0; i < JargonData.length; i++) { 
 
@@ -63,7 +63,7 @@ $.ajax({
 var altTranslations = [];   
    
 $.ajax({
-      url: "Subtitles/Astronomy/Interferometry_Sizing_Up_the_Stars_1_alt_trans.txt",
+      url: "Subtitles/Math/Discrete_Logarithm_Problem_alt_trans.txt",
       success: function (data){altTranslations = data.split('\n');}
 });
    
