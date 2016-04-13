50b6958258055cce00cefd748a4db21b5d6c4442
amkhader <alaa.m.khader@gmail.com>
2016-04-13 12:21:37 +0300

    Update mediaelement-and-player.js

--- mediaelement-master/build/mediaelement-and-player.js
+++ mediaelement-master/build/mediaelement-and-player.js
@@ -46,7 +46,7 @@ var jargon = "";
 //$.get("mediaelement-master/build/jargonList.txt", function(data){items = data.split('\n'); });
 
 $.ajax({
-      url: "mediaelement-master/build/jargonList.txt",
+      url: "Subtitles/Astronomy/Interferometry_Sizing_Up_the_Stars_1_jargon.txt",
       success: function (data){JargonData = data.split('\n'); 
       	for (i = 0; i < JargonData.length; i++) { 
 
@@ -63,7 +63,7 @@ $.ajax({
 var altTranslations = [];   
    
 $.ajax({
-      url: "mediaelement-master/build/Interferometry_Sizing_Up_the_Stars_1_alt_trans.txt",
+      url: "Subtitles/Astronomy/Interferometry_Sizing_Up_the_Stars_1_alt_trans.txt",
       success: function (data){altTranslations = data.split('\n');}
 });
    
